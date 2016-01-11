70f1cd5f02005eee5904af839e462ae0da45434e
Niko <greece93@gmail.com>
2016-01-11 16:10:53 +0200

    added ability to normalize dictionary on creation

--- helper.py
+++ helper.py
@@ -7,11 +7,15 @@ Created on Sun Jan 10 18:01:56 2016
 
 import numpy as np
 
-def createNumberedDictionary(data):
+def createNumberedDictionary(data, normalize=False):
     data = np.unique(data)    
+    if normalize:    
+        step = 1/(len(data)-1)
+    else:
+        step = 1
     entryNr = 0
     dataDirectory = dict()
     for i in range(data.shape[0]):
         dataDirectory[data[i]] = entryNr
-        entryNr += 1
+        entryNr += step
     return dataDirectory    
