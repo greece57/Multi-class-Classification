dc4aaf673bdf53cf7f61eb6f37471a15f497b644
Niko <greece93@gmail.com>
2016-01-24 17:34:38 +0200

    plot Categories

--- VisualizeData.py
+++ VisualizeData.py
@@ -8,6 +8,7 @@ Created on Mon Jan 11 16:38:51 2016
 import matplotlib.pyplot as plt
 import numpy as np
 from helper import readFile
+from helper import createNumberedDictionary
 
 def plotHistogram(data, column = -1, value = 0):
     dataFiltered = data[data[:,column]==value,:]
@@ -24,9 +25,38 @@ def plotHistogram(data, column = -1, value = 0):
     plt.show()
     
     #plot(TripTypes, Occurences)
+    
+def plotCategories(data, columnFeatureX, columnFeatureY):
+    data[data[:,0] > 900, 0] = 0
+    dataFiltered = data[:,(0,columnFeatureX,columnFeatureY)]
+    
+    dictTripTypes = createNumberedDictionary(dataFiltered[:,0])
+    dictTripTypes = dict((y,x) for x,y in dictTripTypes.iteritems())
+
+    featureXDict = createNumberedDictionary(dataFiltered[:,1])
+    featureYDict = createNumberedDictionary(dataFiltered[:,2])    
+    
+    NumberOfTripTypes = len(np.unique(data[:,0])) - 1
+    listTripTypes = []
+    
+    for i in range(0, NumberOfTripTypes):
+        tripType = dictTripTypes[i]
+        dataTripType = dataFiltered[dataFiltered[:,0] == tripType]
+        dataTripType = dataTripType[:,(1,2)]
+        for j in range(0, len(dataTripType)):
+            dataTripType[j,0] = featureXDict[dataTripType[j,0]]
+            dataTripType[j,1] = featureYDict[dataTripType[j,1]]
+        listTripTypes.append(dataTripType)
+    
+    for tripType in listTripTypes:
+        plt.plot(tripType[:,0], tripType[:,1], 'o')
+    plt.show()
+    
+    
 
 
-data = readFile('train.csv')
+data = readFile('train_small.csv')
 
-plotHistogram(data)
-plotHistogram(data, 6, '1017')
+plotCategories(data,5,4)
+#plotHistogram(data)
+#plotHistogram(data, 6, '1017')
