c20555ff51236b1c82610de204f1c793dabded2f
Niko <greece93@gmail.com>
2016-01-28 16:34:47 +0200

    final Version

--- classify.py
+++ classify.py
@@ -1,9 +1,18 @@
 from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
 
-def classify(trainSet, trainLabels, testSet):
+def classifyLogisticRegression(trainSet, trainLabels, testSet):
 	
-	clf = LogisticRegression()
+	clf = LogisticRegression(solver='lbfgs', multi_class='multinomial')
 	clf.fit(trainSet, trainLabels)
 	predictedLabels = clf.predict_proba(testSet)
 
 	return predictedLabels, clf.classes_
+
+def classifyRandomForest(trainSet, trainLabels, testSet):
+    
+    clf = RandomForestClassifier(1000, verbose=2, n_jobs=-1)
+    clf.fit(trainSet, trainLabels)
+    predictedLabels = clf.predict_proba(testSet)
+    
+    return predictedLabels, clf.classes_
